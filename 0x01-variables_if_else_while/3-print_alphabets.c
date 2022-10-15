10cc632df9bda14c709287bc7463c320f36724e5
ENG0553 <chivic0553@gmail.com>
2022-10-15 13:59:59 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/** 
+ * main - print alhpa in lower and upper case
+ * Return: Akways 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
