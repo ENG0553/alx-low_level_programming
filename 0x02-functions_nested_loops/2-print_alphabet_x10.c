b5c6810f67f511461e47808c9a20c912728fc1af
ENG0553 <chivic0553@gmail.com>
2022-10-18 15:22:02 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10x - print 10x
+ */
+void print_alphabet_10x(void)
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 0; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
