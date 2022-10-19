a1fca0406ba471f1daca587cc298bae8dc4ae8c1
ENG0553 <chivic0553@gmail.com>
2022-10-19 14:57:07 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute value
+ * @t: the intergal to be computed
+ * Return: the absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
