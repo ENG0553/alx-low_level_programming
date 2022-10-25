94ce4e5083edf1f07626cb467ac588ba0c2b34b1
ENG0553 <chivic0553@gmail.com>
2022-10-25 01:58:52 +0100

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,13 +6,15 @@
  */
 void puts_half(char *str)
 {
-	int len =0;
+	int len = 0;
 
 	while (*str != '\0')
 	{
 		len++;
 		str++;
 	}
+
+	str -= (len / 2);
 	while (*str != '\0')
 	{
 		_putchar(*str);
